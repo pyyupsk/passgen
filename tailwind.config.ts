17a35bfebe9428bcc50af885fe9c9926755a2b2a
pyyupsk <pyyupsk@proton.me>
2025-03-07 11:21:29 +0700

    feat(styles): add Inter and JetBrains Mono fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,17 @@
 import type { Config } from 'tailwindcss'
 
 import animate from 'tailwindcss-animate'
+import { fontFamily } from 'tailwindcss/defaultTheme'
 
 export default {
   darkMode: ['class'],
   content: ['./index.html', './src/**/*.{ts,js,vue}'],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['var(--font-sans)', ...fontFamily.sans],
+        mono: ['var(--font-mono)', ...fontFamily.mono],
+      },
       borderRadius: {
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
