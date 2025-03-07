e0584c666e841d55685065104ea0345d35c4c7a3
pyyupsk <pyyupsk@proton.me>
2025-03-07 13:13:02 +0700

    refactor(seo): update SEO meta tags with vueuse/head and remove favicon

--- src/composables/use-metadata.ts
+++ src/composables/use-metadata.ts
@@ -1,41 +1,39 @@
 import type { MetadataOptions } from '@/types/MetadataOptions'
 
-import { useHead } from '@vueuse/head'
+import { useHead, useSeoMeta } from '@vueuse/head'
 
-const DEFAULT_AUTHOR = {
-  name: 'pyyupsk',
-  url: 'https://passgen.pyyupsk.vercel.app',
-}
+const AUTHOR = 'pyyupsk'
 const TWITTER_HANDLE = '@pyyupsk_'
 const DEFAULT_IMAGE_DIMENSIONS = { height: 630, width: 1200 }
 
 export const useMetadata = ({ title, description, image }: MetadataOptions) => {
   const imageMeta = image
     ? [
-        { property: 'og:image', content: image },
-        { property: 'og:image:alt', content: title },
-        { property: 'og:image:width', content: String(DEFAULT_IMAGE_DIMENSIONS.width) },
-        { property: 'og:image:height', content: String(DEFAULT_IMAGE_DIMENSIONS.height) },
-        { name: 'twitter:image', content: image },
+        {
+          url: image,
+          width: DEFAULT_IMAGE_DIMENSIONS.width,
+          height: DEFAULT_IMAGE_DIMENSIONS.height,
+          alt: title,
+        },
       ]
-    : []
+    : undefined
+
+  useSeoMeta({
+    title,
+    description,
+    ogDescription: description,
+    ogTitle: title,
+    ogImage: imageMeta,
+    twitterTitle: title,
+    twitterDescription: description,
+    twitterImage: imageMeta,
+    twitterCreator: TWITTER_HANDLE,
+    twitterCard: 'summary_large_image',
+    author: AUTHOR,
+    publisher: AUTHOR,
+  })
 
   useHead({
-    title,
-    meta: [
-      { name: 'description', content: description },
-      { name: 'author', content: DEFAULT_AUTHOR.name },
-      { name: 'publisher', content: DEFAULT_AUTHOR.name },
-      { property: 'og:description', content: description },
-      { property: 'og:site_name', content: title },
-      { property: 'og:title', content: title },
-      { property: 'og:type', content: 'website' },
-      { name: 'twitter:card', content: 'summary_large_image' },
-      { name: 'twitter:creator', content: TWITTER_HANDLE },
-      { name: 'twitter:title', content: title },
-      { name: 'twitter:description', content: description },
-      ...imageMeta,
-    ],
     link: [
       {
         rel: 'icon',
